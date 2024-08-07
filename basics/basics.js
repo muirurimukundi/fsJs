be818f8ed2b33b731c6fd24517bca14f7597eb5c
muirurimukundi <themasalacollective@gmail.com>
2024-08-07 07:36:42 -0400

    Variable usage

--- basics/basics.js
+++ basics/basics.js
@@ -1,2 +1,18 @@
-console.log("Hello ");
-console.log("World");
+console.log( "Hello " );
+console.log( "World" );
+
+
+function says( message ){
+    console.log(message);
+}
+
+console.log( "Hello World" );
+// boolean data type: true , false 
+
+2 + 3 
+console.log(2 + 3);
+
+// typeof operator checks the type of the data structure
+console.log( typeof( true) );
+console.log( typeof( 23) );
+console.log( typeof( "masalamind" ) );
---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+// code below because node.js doesn't use prompt since it's a browser window method
+// prompt( "What is your name?" );
+
+// variables have to be defined and initialized before they're used 
+
+let name = "Jack Robinson";
+
+console.log( name );
