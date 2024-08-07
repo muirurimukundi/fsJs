8b25c98d3253f7058af1cd006725179299b448e5
muirurimukundi <themasalacollective@gmail.com>
2024-08-07 08:05:09 -0400

    Add code to swap values of two variables

--- basics/variable.js
+++ basics/variable.js
@@ -6,3 +6,28 @@
 let name = "Jack Robinson";
 
 console.log( name );
+
+// Exercise 
+
+function test() {
+  var a = "3";
+  var b = "8";
+
+  console.log(`Inside the function, the value of a is ${a} and the value of b is ${b}`)
+}
+
+test();
+
+let a = 11,
+  b = 6;
+
+console.log( `Outside, initially the value of a is ${a}, while value of b is ${b}` );
+
+// assignment is to swap the values, introduce a third variable to hold one of the values
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log( `Now the new value of a is ${a}, whereas the value of b is ${b}` );
